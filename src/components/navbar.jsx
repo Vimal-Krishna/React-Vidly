50b5d0dda7845393067e1c87deabf5489a052fa9
Vimal Krishna <vimal.krishna.84@gmail.com>
2022-06-11 22:01:21 +0530

    navbar-expand

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 const NavBar = (props) => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <nav className="navbar navbar-expand navbar-light bg-light">
             <NavLink className="navbar-brand" to="/">
                 Vidly
             </NavLink>
